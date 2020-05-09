1a23b6f3be94b822ee50aaec5d3343fe01473efe
Savan Visalpara <savan77@users.noreply.github.com>
2020-05-09 13:55:02 -0500

    Add files via upload

---
+++ data_generator.py
@@ -0,0 +1,33 @@
+###
+# Generate data to train the BERT model
+
+import csv
+import json
+import argparse
+import os
+
+def create_model(llabels):
+	llist = [0]* 12
+	for em, val in enumerate(llabels.values()):
+		llist[em] = 1 if val else 0
+	return llist
+
+def generate_csv(file, csvfile):
+	data= open("nlp_train.json","r")
+	out = open(csvfile, "w", encoding="utf-8", newline="")
+	writer = csv.writer(out)
+	writer.writerow(["id", "text", "anger", "anticipation","disgust","fear","joy","love","optimism","pessimism","sadness","surprise","trust","neutral"])
+	data = json.load(data)
+	idd = 0
+	for i,v in data.items():
+		bin_vector = create_model(v['emotion'])
+		writer.writerow([idd,v['body']]+bin_vector)
+		idd += 1
+	
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--file", default="nlp_train.json", type=str)
+	parser.add_argument("--csvfile", default="nlp_train.csv", type=str)
+	args = parser.parse_args()
+	generate_csv(args.file, args.csvfile)
